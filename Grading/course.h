d1b64999e4d1084362e7be6b0764a5b7a55e3c3f
@nsengiyumv33 <nsengiyumv33@gmail.com>
2022-07-03 12:30:57 +0200

    feat: defining all the methods

--- Grading/course.h
+++ Grading/course.h
@@ -2,6 +2,7 @@
 #define H_Course
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -15,7 +16,8 @@ private:
 
 public:
     Course();
-    Course(string name = "", string number="", char grade='*', int details=0);
+    Course(string name = "", string number = "", char grade = '*', int details = 0);
+    void setCourseInfor(string name, string number, char grade, int details);
     void setCourseName(string name);
     void setCourseNumber(string number);
     void setCourseGrade(char grade);
@@ -24,17 +26,111 @@ public:
     string getCourseNumber();
     char getCourseGrade();
     int getCourseDetails();
-    void print(ostream& outp, bool isGrade);
+    void print(ostream &outp, bool isGrade);
 
-
-    bool operator==(Course& c);
-    bool operator!=(Course& c);
-    bool operator<(Course& c);
-    bool operator>(Course& c);
-    bool operator<=(Course& c);
-    bool operator>=(Course& c);
-
-
+    bool operator==(Course &c);
+    bool operator!=(Course &c);
+    bool operator<(Course &c);
+    bool operator>(Course &c);
+    bool operator<=(Course &c);
+    bool operator>=(Course &c);
 };
 
+// definitins
+Course::Course()
+{
+    courseName = "";
+    courseNumber = "";
+    courseGrade = '*';
+    courseDetails = 0;
+}
+
+void Course::setCourseInfor(string name, string number, char grade, int details)
+{
+    courseName = name;
+    courseNumber = number;
+    courseGrade = grade;
+    courseDetails = details;
+}
+
+void Course::print(ostream &outp, bool isGrade)
+{
+    outp << left;
+    outp << setw(8) << courseNumber << " ";
+    outp << setw(20) << courseName << " ";
+    outp.unsetf(ios::left);
+    outp << setw(20) << courseDetails << " ";
+
+    if (isGrade)
+    {
+        outp << setw(4) << courseGrade << " ";
+    }
+    else
+    {
+        outp << setw(4) << " ***";
+    }
+}
+
+// setters
+void Course::setCourseName(string name)
+{
+    courseName = name;
+}
+
+void Course::setCourseDetails(int details)
+{
+    courseDetails = details;
+}
+
+void Course::setCourseNumber(string number)
+{
+    courseNumber = number;
+}
+
+void Course::setCourseGrade(char grade)
+{
+    courseGrade = grade;
+}
+
+// getters
+string Course::getCourseName()
+{
+    return courseName;
+}
+
+string Course::getCourseNumber()
+{
+    return courseNumber;
+}
+
+char Course::getCourseGrade()
+{
+    return courseGrade;
+}
+
+int Course::getCourseDetails()
+{
+    return courseDetails;
+}
+
+
+// define operators
+bool Course::operator==(Course &c){
+    return (courseNumber == c.courseNumber);
+}
+bool Course::operator!=(Course &c){
+    return (courseNumber != c.courseNumber);
+}
+bool Course::operator<(Course &c){
+    return (courseNumber < c.courseNumber);
+}
+bool Course::operator>(Course &c){
+    return (courseNumber > c.courseNumber);
+}
+bool Course::operator<=(Course &c){
+    return (courseNumber <= c.courseNumber);
+}
+bool Course::operator>=(Course &c){
+    return (courseNumber >= c.courseNumber);
+}
 #endif
